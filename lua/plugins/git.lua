bafaf927fb53099901f30ac7c1367e1cfbecb555
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-02-26 03:44:14 -0300

    remove unwanted plugins

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,9 +1,4 @@
 return { -- Adds git related signs to the gutter, as well as utilities for managing changes
-  {
-    'lewis6991/gitsigns.nvim',
-    opts = {},
-    vim.keymap.set('n', '<leader>y', ':Gitsigns preview_hunk<CR>', { desc = 'Display Git Hunk', silent = true }),
-  },
-  { 'tpope/vim-fugitive' },
-  { 'sindrets/diffview.nvim' }, -- optional - Diff integration
+  'lewis6991/gitsigns.nvim',
+  opts = {},
 }
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -22,13 +22,5 @@ return { -- Useful plugin to show you pending keybinds.
         align = 'left', -- align columns left, center or right
       },
     }
-    -- Document existing key chains
-    require('which-key').register {
-      -- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-      -- ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-      -- ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-      -- ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-    }
   end,
 }

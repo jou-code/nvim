3a252d7a5aaf9f5036f5147c7ec2d1fa83c5e60e
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-02-26 22:10:45 -0300

    update path

--- lua/colors.lua
+++ lua/colors.lua
@@ -1,5 +1,5 @@
 -- Themery block
 -- This block will be replaced by Themery.
-vim.cmd("colorscheme tokyonight-moon")
+vim.cmd("colorscheme tokyonight-night")
 vim.g.theme_id = 1
 -- end themery block
--- lua/plugins/themery.lua
+++ lua/plugins/themery.lua
@@ -1,15 +1,25 @@
 return {
-  {'zaldih/themery.nvim',
+  {
+    'zaldih/themery.nvim',
     config = function()
       -- Minimal config
-require("themery").setup({
-  themes = {"tokyonight-moon", 'tokyonight-night',  'tokyonight-storm', 'catppuccin-frappe', 'catppuccin-macchiato', 'catppuccin-mocha', 'onedark', 'oxocarbon'},
-  -- themeConfigFile = "~/.config/nvim/lua/theme.lua", -- Described below
-  themeConfigFile = "~/.config/momovim/lua/colors.lua", -- Described below
-  livePreview = true, -- Apply theme while browsing. Default to true.
-})
-    end
+      require('themery').setup {
+        themes = {
+          'tokyonight-moon',
+          'tokyonight-night',
+          'tokyonight-storm',
+          'catppuccin-frappe',
+          'catppuccin-macchiato',
+          'catppuccin-mocha',
+          'onedark',
+          'oxocarbon',
+        },
+        -- themeConfigFile = "~/.config/nvim/lua/theme.lua", -- Described below
+        themeConfigFile = '~/.config/nvim/lua/colors.lua', -- Described below
+        livePreview = true, -- Apply theme while browsing. Default to true.
+      }
+    end,
   },
-vim.keymap.set('n', '<leader>0', ':Themery<CR>', { desc = 'Theme Selection', silent = true }),
-vim.keymap.set('n', '<F5>', ':Themery<CR>', { desc = 'Theme Selection', silent = true })
+  vim.keymap.set('n', '<leader>0', ':Themery<CR>', { desc = 'Theme Selection', silent = true }),
+  vim.keymap.set('n', '<F5>', ':Themery<CR>', { desc = 'Theme Selection', silent = true }),
 }
